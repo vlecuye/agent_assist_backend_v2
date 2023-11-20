d71dd32f1fbc9c5caf4e30f5f3bc868e3f3fe4f6
Vincent Lecuyer <vlecuyer@google.com>
2023-11-20 13:13:21 -0500

    initial commit>

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from flask import Flask,request,jsonify
 import json
 import datetime
 from google.cloud import dialogflow_v2beta1 as dialogflow
+from google.oauth2 import service_account
 import os
 from flask_cors import CORS
-import jsonpickle
+
 app = Flask(__name__)
 CORS(app,resources={r"/*": {"origins": "*"}})
 conversations = []
-
+SERVICE_ACCOUNT_FILE = 'quebec-ccai-demo-81db958564a7.json'
+project_id='quebec-ccai-demo'
+credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE)
 @app.get('/conversation/list')
 def list_conversation():
     return json.dumps(conversations);
@@ -16,7 +19,7 @@ def list_conversation():
 @app.post('/conversation/create')
 def create_conversation():
     content = request.json
-    response = _create_conversation(content['project_id'],content['conversation_profile_id'])
+    response = _create_conversation(project_id,content['conversation_profile_id'])
     conversations.append(response)
     return json.dumps(response)
 
@@ -29,7 +32,7 @@ def _create_conversation(project_id, conversation_profile_id):
         project_id:  The GCP project linked with the conversation.
         conversation_profile_id: The conversation profile id used to create
         conversation."""
-    client = dialogflow.ConversationsClient()
+    client = dialogflow.ConversationsClient(credentials=credentials)
     conversation_profile_client = dialogflow.ConversationProfilesClient()
     project_path = client.common_project_path(project_id)
     conversation_profile_path = conversation_profile_client.conversation_profile_path(
@@ -39,7 +42,7 @@ def _create_conversation(project_id, conversation_profile_id):
     response = client.create_conversation(
         parent=project_path, conversation=conversation
     )
-
+    print(response)
     print("Life Cycle State: {}".format(response.lifecycle_state))
     print("Conversation Profile Name: {}".format(response.conversation_profile))
     print("Name: {}".format(response.name))
@@ -48,7 +51,7 @@ def _create_conversation(project_id, conversation_profile_id):
 @app.post('/participant/create')
 def create_participant():
     content = request.get_json()
-    response = _create_participant(content['project_id'],content['conversation_id'],content['role'])
+    response = _create_participant(project_id,content['conversation_id'],content['role'])
     return json.dumps(response)
 
 def _create_participant(project_id, conversation_id, role):
@@ -59,7 +62,7 @@ def _create_participant(project_id, conversation_id, role):
         conversation_id: Id of the conversation.
         participant: participant to be created."""
 
-    client = dialogflow.ParticipantsClient()
+    client = dialogflow.ParticipantsClient(credentials=credentials)
     conversation_path = dialogflow.ConversationsClient.conversation_path(
         project_id, conversation_id
     )
@@ -74,10 +77,15 @@ def _create_participant(project_id, conversation_id, role):
 
 @app.post('/conversation/analyze')
 def analyze_content_text():
-    content = request.get_json()
-    response = _analyze_content_text(content['participant_id'],content['text'])
+
+    if request.form.get('type') == 'text' : 
+        message = request.form.get('text')
+    else :
+        message=request.files.get('blob')
+        print(message)
+    response = _analyze_content(request.form.get('participant_id'),message,request.form.get('type'))
     return json.dumps(response)
-def _analyze_content_text(participant_id, text):
+def _analyze_content(participant_id, content,contentType):
     """Analyze text message content from a participant.
 
     Args:
@@ -85,63 +93,56 @@ def _analyze_content_text(participant_id, text):
         conversation_id: Id of the conversation.
         participant_id: Id of the participant.
         text: the text message that participant typed."""
+    if contentType == "text": 
+        request = dialogflow.StreamingAnalyzeContentRequest(
+                text_config= dialogflow.InputTextConfig(language_code="fr-ca"),
+                participant=participant_id,
+            )
+        request2 = dialogflow.StreamingAnalyzeContentRequest(input_text=content)
+    else : 
+        request=dialogflow.StreamingAnalyzeContentRequest(
+            participant=participant_id,
+            audio_config=dialogflow.InputAudioConfig(audio_encoding=dialogflow.AudioEncoding.AUDIO_ENCODING_OGG_OPUS,sample_rate_hertz=48000,language_code="fr-ca"))
+        bytes = content.read()
+        request2=dialogflow.StreamingAnalyzeContentRequest(input_audio=bytes)                                    
 
-    client = dialogflow.ParticipantsClient()
-    text_input = {"text": text, "language_code": "en-US"}
-    response = client.analyze_content(
-        participant=participant_id, text_input=text_input
-    )
-    print("AnalyzeContent Response:")
-    print("Reply Text: {}".format(response.reply_text))
-    print(response)
-    payload = {"articles":[],"faqs":[]}
-    payload['send_time'] = datetime.datetime.fromtimestamp(response.message.send_time.timestamp()).strftime('%Y-%m-%d %H:%M:%S.%f')
-    payload['content'] = response.message.content
-    payload['participant_type'] = response.message.participant_role
-    for suggestion_result in response.human_agent_suggestion_results:
-        if suggestion_result.error is not None:
-            print("Error: {}".format(suggestion_result.error.message))
-        if suggestion_result.suggest_articles_response:
-            for answer in suggestion_result.suggest_articles_response.article_answers:
-                payload['articles'].append({"title":answer.title,"uri":answer.uri,"send_time":payload['send_time'],"snippet":answer.snippets[0]})
-                print(answer)
-                print("Article Suggestion Answer: {}".format(answer.title))
-                print("Answer Record: {}".format(answer.answer_record))
-        if suggestion_result.suggest_faq_answers_response:
-            for answer in suggestion_result.suggest_faq_answers_response.faq_answers:
-                print("Faq Answer: {}".format(answer.answer))
-                print("Answer Record: {}".format(answer.answer_record))
-        if suggestion_result.suggest_smart_replies_response:
-            for (
-                answer
-            ) in suggestion_result.suggest_smart_replies_response.smart_reply_answers:
-                print("Smart Reply: {}".format(answer.reply))
-                print("Answer Record: {}".format(answer.answer_record))
+    requests = [request,request2]
+    client = dialogflow.ParticipantsClient(credentials=credentials)
+    def request_generator():
+            for request in requests:
+                yield request
 
-    for suggestion_result in response.end_user_suggestion_results:
-        if suggestion_result.error:
-            print("Error: {}".format(suggestion_result.error.message))
-        if suggestion_result.suggest_articles_response:
-            for answer in suggestion_result.suggest_articles_response.article_answers:
-                print("Article Suggestion Answer: {}".format(answer.title))
-                print("Answer Record: {}".format(answer.answer_record))
-        if suggestion_result.suggest_faq_answers_response:
-            for answer in suggestion_result.suggest_faq_answers_response.faq_answers:
-                print("Faq Answer: {}".format(answer.answer))
-                print("Answer Record: {}".format(answer.answer_record))
-        if suggestion_result.suggest_smart_replies_response:
-            for (
-                answer
-            ) in suggestion_result.suggest_smart_replies_response.smart_reply_answers:
-                print("Smart Reply: {}".format(answer.reply))
-                print("Answer Record: {}".format(answer.answer_record))
-
+        # Make the request
+    stream = client.streaming_analyze_content(requests=request_generator())
+    for response in stream:
+        print(response)
+        payload = {"text":'',"articles":[]}
+    now  = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    for agent_response in response.automated_agent_reply.response_messages:
+        if agent_response.text:
+            print(agent_response)
+            payload['text'] = agent_response.text.text[0]
+            payload['send_time'] = now
+            print(payload)
+        if agent_response.payload:
+            print(agent_response)
+            for richContentResponses in agent_response.payload.get('richContent') :
+                for richContentResponse in richContentResponses : 
+                    citations = richContentResponse.get('citations')
+                    for citation in citations :
+                        article = {}
+                        article['title'] = citation.get('title')
+                        article['snippet']= citation.get('subtitle')
+                        article['uri'] = citation.get('actionLink')
+                        article['send_time'] = now
+                        payload['articles'].append(article)
     return payload
+    
 
 @app.post('/conversation/complete')
 def complete_conversation():
     content = request.get_json()
-    _complete_conversation(content['project_id'],content['conversation_id'])
+    _complete_conversation(project_id,content['conversation_id'])
 
 def _complete_conversation(project_id, conversation_id):
     """Completes the specified conversation. Finished conversations are purged from the database after 30 days.
